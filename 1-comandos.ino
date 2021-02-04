a4b71e699fc831a76e77f748a65427c2061c082c
Abraham Rodriguez <38869805+Arendost@users.noreply.github.com>
2021-02-04 13:08:05 -0400

    Create 1-comandos.ino

---
+++ 1-comandos.ino
@@ -0,0 +1,79 @@
+void comandoSelecto()
+{
+
+
+  if (str == "el")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+
+
+  if (str == "al")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "tad")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "tat")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "maa")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "maat")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mba")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mbat")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mca")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mcat")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mda")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+  if (str == "mdat")
+  {
+    Serial.println(str);
+    conversionEnvio();
+    limpiar();
+  }
+}
